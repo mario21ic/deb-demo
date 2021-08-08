ca70b4c1ea5289627aeae5c7295af6b5d3f91869
Mario Inga <mario21ic@gmail.com>
2021-08-08 01:46:01 -0500

    Adding pre and post installation and remove

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-docker run --rm --name test-deb -d --platform=linux/amd64 -v $PWD:/app ubuntu:16.04 sleep 8000
+docker run --name test-deb -d --platform=linux/amd64 -v $PWD:/app ubuntu:16.04 sleep 8000
+
 docker exec -ti test-deb dpkg -i /app/hello_1.0-1_amd64.deb
-docker exec -ti test-deb hello
 docker exec -ti test-deb which hello
+docker exec -ti test-deb hello
+
+docker exec -ti test-deb apt-get remove --yes hello
 
 docker rm -f test-deb
 
